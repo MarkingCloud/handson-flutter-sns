cdcb5d66faaaa152e34c2d4066237df972d7175f
kou72 <genpokopokopoko@gmail.com>
2021-09-24 20:33:28 +0900

    android用の設定追加＆codemagicで自動ビルド

--- lib/model/auth_model.dart
+++ lib/model/auth_model.dart
@@ -1,14 +1,28 @@
 // 依存パッケージ
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 final authModelProvider = StreamProvider.autoDispose((ref) {
   return FirebaseAuth.instance.authStateChanges();
 });
 
 Future signInAuth() async {
-  GoogleAuthProvider googleProvider = GoogleAuthProvider();
-  return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+  try {
+    // Natibe用
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  } catch (e) {
+    // Web用
+    GoogleAuthProvider googleProvider = GoogleAuthProvider();
+    return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+  }
 }
 
 Future signOutAuth() async {
